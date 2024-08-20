f20b46ac3d5847a2efb7b2d6b2d0726109a34df1
Lucas dos Santos Soares <129183068+17071996-Lucas-dos-Santos-Soares@users.noreply.github.com>
2024-08-20 18:11:51 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -258,3 +258,20 @@ revelar.reveal('.local', {
   delay: 600,
   origin: 'bottom'
 })
+
+// rolar para cima
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("scrollTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollTopBtn").style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
