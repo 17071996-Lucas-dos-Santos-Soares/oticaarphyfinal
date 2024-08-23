bb336da4eb4f5b92b41381392d74fb173d696734
Lucas dos Santos Soares <129183068+17071996-Lucas-dos-Santos-Soares@users.noreply.github.com>
2024-08-23 10:09:01 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -258,16 +258,3 @@ revelar.reveal('.local', {
   delay: 600,
   origin: 'bottom'
 })
-
-window.onscroll = function() {
-  let scrollToTopBtn = document.querySelector("#scrollToTopBtn");
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      scrollToTopBtn.style.display = "block";
-  } else {
-      scrollToTopBtn.style.display = "none";
-  }
-};
-
-document.querySelector("#scrollToTopBtn").onclick = function() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-};
